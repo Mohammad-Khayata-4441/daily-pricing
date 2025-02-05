e9c5f6829921ec11ad543183460b3af7d86cf052
Mohammad Khayata <mohammadKhayata.gm@gmail.com>
2025-02-05 20:22:20 +0300

    pricing

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,12 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Almarai } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
+const almarai = Almarai({
   variable: "--font-geist-mono",
   subsets: ["latin"],
+  style: "normal",
+  weight: ["400"],
 });
 
 export const metadata: Metadata = {
@@ -23,10 +20,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+    <html lang="ar" dir="rtl">
+      <body className={`${almarai.variable} ${almarai.variable} antialiased`}>
         {children}
       </body>
     </html>
--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,95 @@
 import Image from "next/image";
+import { data } from "./data";
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const currency = "ل.س";
+  const date = new Date().toLocaleDateString();
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+  const categories = data;
+
+  return (
+    <div className="items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <header className="relative h-[300px] w-full">
+        <Image
+          className="object-contain object-center"
+          src={"/h1.jpg"}
+          alt="free syria"
+          fill
+        />
+      </header>
+      <main className="grid gap-8 row-start-2 items-center sm:items-start">
+        <div className="w-full col-span-2">
+          <div className="w-full flex justify-between items-center mb-3 mt-1 pl-3">
+            <div>
+              <h3 className="text-lg font-semibold text-slate-800">
+                أسعار المواد الأساسية اليوم
+              </h3>
+              <p className="text-gray-600 ">
+                متوسط الأسعار ضمن مختلف المحافظات.
+              </p>
+              <p className="text-gray-600 ">
+                لتاريخ <span className="font-bold mx-2">{date}</span>
+              </p>
+            </div>
+          </div>
+          {categories.map((category) => (
+            <div key={category.title} className="mb-10">
+              <h2 className="text-xl font-bold mb-3 text-slate-900">
+                {category.title}
+              </h2>
+              <div className="relative flex flex-col w-full text-gray-700 bg-white shadow-md rounded-lg bg-clip-border">
+                <table className="text-right table-auto w-full">
+                  <thead>
+                    <tr className="border-b border-slate-300 bg-slate-50">
+                      <th className="p-4 text-lg font-normal leading-none">
+                        صورة
+                      </th>
+                      <th className="p-4 text-lg font-normal leading-none">
+                        المادة
+                      </th>
+                      <th className="p-4 text-lg font-normal leading-none">
+                        أقل سعر
+                      </th>
+                      <th className="p-4 text-lg font-normal leading-none">
+                        متوسط السعر
+                      </th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {category.products.map((pr) => (
+                      <tr className="hover:bg-slate-50" key={pr.id}>
+                        <td className="p-4 border-b border-slate-200 py-5">
+                          <img
+                            src={pr.image}
+                            alt={pr.name}
+                            className="w-16 h-16 object-cover rounded"
+                          />
+                        </td>
+                        <td className="p-4 border-b border-slate-200 py-5">
+                          <p className="block font-semibold text-lg text-slate-800">
+                            {pr.name} ({pr.quantity} {pr.unit})
+                          </p>
+                        </td>
+                        <td className="p-4 border-b border-slate-200 py-5">
+                          <p className="text-lg">
+                            {pr.minPrice} {currency}
+                          </p>
+                        </td>
+                        <td className="p-4 border-b border-slate-200 py-5">
+                          <p className="text-lg">
+                            {pr.avgPrice} {currency}
+                          </p>
+                        </td>
+                      </tr>
+                    ))}
+                  </tbody>
+                </table>
+              </div>
+            </div>
+          ))}
         </div>
       </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center"></footer>
     </div>
   );
 }
